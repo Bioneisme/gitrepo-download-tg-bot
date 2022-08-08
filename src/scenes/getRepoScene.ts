06ec23ac940fb5a2a2a4550b823efc46d8ad7482
Bioneisme <bioneisme@gmail.com>
2022-08-08 17:03:18 +0600

    Getting repository scene

---
+++ src/scenes/getRepoScene.ts
@@ -0,0 +1,28 @@
+import {Scenes} from "telegraf";
+import {Message} from "typegram";
+import commands from "../commands/main";
+
+let username: string;
+let repo: string;
+
+export const getRepoScene = new Scenes.WizardScene(
+    'getRepo',
+    async (ctx) => {
+        await ctx.reply('Username: ');
+        return ctx.wizard.next();
+    },
+    async (ctx) => {
+        if (ctx.message) {
+            username = (ctx.message as Message.TextMessage).text;
+            await ctx.reply('Repository name: ')
+            return ctx.wizard.next();
+        }
+    },
+    async (ctx) => {
+        if (ctx.message) {
+            repo = (ctx.message as Message.TextMessage).text;
+            await ctx.scene.leave();
+            return commands.downloadRepo(ctx, username, repo);
+        }
+    }
+)
