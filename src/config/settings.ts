72cefd9f4ced6387a6409aa33cb73d0ca784fa0c
Bioneisme <bioneisme@gmail.com>
2022-08-09 16:23:53 +0600

    Changed default postgres values

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const BOT_TOKEN: string = process.env.BOT_TOKEN as string;
-const PG_USER: string = process.env.PG_USER || 'postgres';
-const PG_HOST: string = process.env.PG_HOST || 'localhost';
-const PG_PASSWORD: string = process.env.PG_PASSWORD || 'root';
-const PG_DATABASE: string = process.env.PG_DATABASE || 'repo_bot';
-const PG_PORT: number = +(process.env.PG_PORT || '5432');
+const PG_USER: string = process.env.POSTGRES_USER || 'postgres';
+const PG_HOST: string = process.env.POSTGRES_HOST || 'db';
+const PG_PASSWORD: string = process.env.POSTGRES_PASSWORD || 'root';
+const PG_DATABASE: string = process.env.POSTGRES_DB || 'repo_bot';
+const PG_PORT: number = +(process.env.POSTGRES_PORT || '5432');
 
 const UPD_TIME: number = 7 // days
 
