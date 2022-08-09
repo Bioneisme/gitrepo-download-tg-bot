a241423150074d9060cb5280952dc2019ddac4b3
Bioneisme <bioneisme@gmail.com>
2022-08-09 12:09:22 +0600

    Added days to update file

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -7,8 +7,9 @@ const PG_USER: string = process.env.PG_USER || 'postgres';
 const PG_HOST: string = process.env.PG_HOST || 'localhost';
 const PG_PASSWORD: string = process.env.PG_PASSWORD || 'root';
 const PG_DATABASE: string = process.env.PG_DATABASE || 'repo_bot';
-const PG_PORT: number = parseInt(process.env.PG_PORT || '5432');
+const PG_PORT: number = +(process.env.PG_PORT || '5432');
 
+const UPD_TIME: number = 7 // days
 
 export const BOT = {
     token: BOT_TOKEN
@@ -22,9 +23,14 @@ export const PG = {
     port: PG_PORT
 };
 
+export const CFG = {
+    days: UPD_TIME
+}
+
 const config = {
     bot: BOT,
-    postgres: PG
+    postgres: PG,
+    cfg: CFG
 };
 
 export default config;
